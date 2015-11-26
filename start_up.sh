098be993ba8518a4150220b96023b80208da44d7
danielsauceda <danielsauceda34@gmail.com>
2015-11-26 00:02:05 -0600

    update

--- start_up.sh
+++ start_up.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+
+
+
+
 clear
 echo "welcome, $USER to the start up script"
 
@@ -8,77 +13,69 @@ echo "your sudo settings"
 echo "we will now add recommended repositories"
 sleep 5
 
-#add-apt-repository ppa:nilarimogard/webupd8
-#add-apt-repository ppa:nilarimogard/webupd8
-#add-apt-repository ppa:webupd8team/y-ppa-manager
-#add-apt-repository ppa:ubuntu-wine/ppa
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo apt-add-repository ppa:xorg-edgers/ppa
+sudo apt-add-repository ppa:ubuntu-x-swat/x-updates
+sudo apt-get update
+sudo apt-get -y install nvidia-current nvidia-settings
 
-#sudo -E wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update
+
+sudo -E wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update
 
 echo "We will now update and upgrade your packages"
 sleep 5
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
 echo "We will now make recommended installs"
 sleep 5
 apt-get -y install -f
 
-apt-get -y install wget
-apt-get -y install curl
-apt-get -y install synaptic
-sudo apt-get install android-tools-adb
-#apt-get --assume-yes install y-ppa-manager
+sudo apt-get -y install wget
+sudo apt-get -y install curl
+sudo apt-get -y install synaptic
+sudo apt-get --assume-yes install y-ppa-manager
+sudo apt-get -y android-tools-adb
 
-cd ~/Downloads && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - dropbox.deb 
-~/Downloads/.dropbox-dist/dropboxd
-apt-get -y install kate
-
-apt-get -y install  chromium-browser
-
-
-#install pycharm
-wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
-sudo apt-get update
-sudo apt-get install pycharm
 
 #install oracle Java
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get -y install oracle-java8-installer
 
 
 
 
+sudo apt-get -y install emacs
+sudo apt-get -y install git
+sudo apt-get -y install guake
+sudo apt-get -y install openssh-server
+sudo apt-get -y install zlib1g-dev 
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install build-essential
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libxml2-dev
+sudo apt-get -y install openssl
+sudo apt-get -y install libxslt1-dev
+sudo apt-get -y install python-dev
+sudo apt-get -y install libzmq-dev
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install iptables
+sudo apt-get -y install tree
+sudo apt-get -y install pylint
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install virtualenvwrapper
+sudo apt-get -y install apache2
+sudo apt-get -y install python-pip
 
 
 
-
-apt-get -y install zlib1g-dev 
-apt-get -y install python-setuptools
-apt-get -y install build-essential
-apt-get -y install libssl-dev
-apt-get -y install libxml2-dev
-apt-get -y install openssl
-apt-get -y install libxslt1-dev
-apt-get -y install python-dev
-apt-get -y install libzmq-dev
-apt-get -y install libpq-dev
-apt-get -y install iptables
-apt-get -y install tree
-apt-get -y install pylint
-apt-get -y install openjdk-7-jdk
-apt-get -y install virtualenvwrapper
-apt-get -y install apache2
-apt-get -y install python-pip
-
-
-
-#apt-get -y install -f
-#apt-get -y update 
-#apt-get -y upgrade 
+sudo apt-get -y install -f
+sudo apt-get -y update 
+sudo apt-get -y upgrade 
 
 echo " ALL DONE :) "
 
