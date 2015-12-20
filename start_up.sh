6f4ba851a908222ff7c429e8ca97a9d555f25019
danielsauceda <danielsauceda34@gmail.com>
2015-12-19 22:34:17 -0600

    adding powertop

--- start_up.sh
+++ start_up.sh
@@ -80,6 +80,7 @@ sudo apt-get -y install latex-beamer
 sudo apt-get -y install impressive
 sudo apt-get -y install pixz
 sudo apt-get -y install libfreetype6-dev libxft-dev
+sudo apt-get -y install powertop
 
 sudo apt-get -y install -f
 sudo apt-get -y update 
